9f79d5fb9ef77a36a4b365efc894f6f43fbe799e
Theo <kkw.theo@gmail.com>
2025-04-16 09:50:12 +0900

    test: 체크 예외 기본 이해

---
+++ src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -0,0 +1,56 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(service::callThrow).isInstanceOf(MyCheckedException.class);
+    }
+
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        /**
+         * 예외를 잡아서 처리하는 코드
+         */
+        public void callCatch() {
+            try {
+                repository.call();
+            }catch(MyCheckedException e) {
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+    }
+}
+
